b1f7695016a06f92470ca41dd0eefe4691951568
Student97112 <97112198@st.deltion.nl>
2024-03-07 14:09:15 +0100

    versie1

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -1,7 +1,13 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: Erwin Kapidzic
+# Groep:1xt2
 
 # Hier komt je code...
 
 my_list = ...
+# Maak een lijst van de gegeven waarden
+my_list = [13, "wat een weertje", 12.5, 8]
+
+# Print de lijst
+print(my_list)
+
