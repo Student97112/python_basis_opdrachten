f645d9ea75ac95a8ed0ea9dfb1742b78eb4206b8
Student97112 <97112198@st.deltion.nl>
2024-03-07 14:24:21 +0100

    versie020 b

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -6,4 +6,11 @@
 
 my_list = ...
 
-print()
+# Gegeven tekst
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+
+# Aantal keer dat de letter "t" voorkomt in my_string
+count_t = my_string.count("t")
+
+# Print het resultaat
+print(f"De letter 't' komt {count_t}x voor in my_string")
