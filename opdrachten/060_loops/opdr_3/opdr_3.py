fdd29f7422dd779daf406ae091a306f6920df31c
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:06:53 +0100

    060 opdr3

--- opdrachten/060_loops/opdr_3/opdr_3.py
+++ opdrachten/060_loops/opdr_3/opdr_3.py
@@ -6,4 +6,13 @@
 
 # Hier start de for-loop
 
-my_list = []
+# Maak een lege lijst om de resultaten op te slaan
+resultaten = []
+
+# Gebruik een for-loop en een range-functie om de gewenste berekeningen uit te voeren
+for i in range(3, 82, 3):
+    resultaat = ((i ** 2) / 3)
+    resultaten.append(resultaat)
+
+# Print de gegenereerde lijst op het scherm
+print(resultaten)
