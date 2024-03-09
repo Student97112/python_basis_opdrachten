cc09eda96c762dd6c4f477518642f592b1cfa47a
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:02:38 +0100

    050 opdr2

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -4,4 +4,21 @@
 
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+# Maak een initiële lijst met de gasten
+gasten = ["Jij", "Paul", "Kees", "Marie", "Hilda"]
+
+# Print de initiële lijst op het scherm
+print(gasten)
+
+# Verwijder Marie uit de lijst
+gasten.remove("Marie")
+
+# Print de aangepaste lijst op het scherm
+print(gasten)
+
+# Voeg George naast Kees toe aan de lijst
+kees_index = gasten.index("Kees")
+gasten.insert(kees_index + 1, "George")
+
+# Print de uiteindelijke lijst op het scherm
+print(gasten)
