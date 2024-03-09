8bc91a871419565e8ca9bffcbef7359f310a98ec
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:08:35 +0100

    060 opdr5

--- opdrachten/060_loops/opdr_5/opdr_4.py
+++ opdrachten/060_loops/opdr_5/opdr_4.py
@@ -6,4 +6,8 @@
 
 # Hier start de for-loop
 
-my_list = []
+# Gebruik een lijstcomprehensie om de eerste 6 getallen te genereren
+resultaten = [x * 3 for x in range(1, 21)][:6]
+
+# Print de eerste 6 getallen
+print(resultaten)
