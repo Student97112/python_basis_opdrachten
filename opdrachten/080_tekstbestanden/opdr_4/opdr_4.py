5369985513f66f5bb7c6a474761d7da3b75dcff6
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:16:31 +0100

    080 opdr4

--- opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
+++ opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
@@ -4,3 +4,28 @@
 
 
 # Party enquete
+
+# Vragenlijst
+vragen = [
+    "Wat is je voornaam?",
+    "Wat is je achternaam?",
+    "Wat neem je mee aan drank?",
+    "Wat neem je mee om te eten?"
+]
+
+# Open een tekstbestand om de antwoorden op te slaan (of maak het bestand als het niet bestaat)
+with open("feestgangers.txt", "a") as file:
+    # Loop door de vragen en vraag antwoorden aan de gebruiker
+    antwoorden = {}
+    for vraag in vragen:
+        antwoord = input(vraag + "\n")
+        antwoorden[vraag] = antwoord
+    
+    # Schrijf de antwoorden naar het tekstbestand
+    file.write("----\n")
+    for vraag, antwoord in antwoorden.items():
+        file.write(f"{vraag.lower()}: {antwoord}\n")
+    file.write("----\n")
+
+print("Bedankt voor het invullen!")
+print("See you at the party.")
