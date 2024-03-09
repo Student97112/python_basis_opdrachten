1ab7bcc0f66a100bcf7cf9ab84357a4af4626f8a
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:03:28 +0100

    050 opdr3

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -4,3 +4,16 @@
 
 # Hier komt je code...
 
+# Vraag de gebruiker om een lijst met objecten in te vullen en splits de input op komma's
+input_string = input("Voer minimaal 5 objecten gescheiden door komma's in: ")
+objecten = input_string.split(',')
+
+# Strip eventuele spaties aan het begin en einde van elk object
+objecten = [object.strip() for object in objecten]
+
+# Sorteer de lijst in omgekeerde volgorde
+objecten.sort(reverse=True)
+
+# Print de gesorteerde lijst op het scherm
+print(objecten)
+
