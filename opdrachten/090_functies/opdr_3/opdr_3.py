1fc9c48bd03f418ffef873d855832f788954520d
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:20:17 +0100

    090 opdr3

--- opdrachten/090_functies/opdr_3/opdr_3.py
+++ opdrachten/090_functies/opdr_3/opdr_3.py
@@ -3,18 +3,20 @@
 # Groep:
 
 
+import math
+
 def kubus_vol(m):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    """Bereken het volume van een kubus met zijden van lengte m."""
+    volume = m ** 3
+    return volume
 
 def bol_vol(r):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    """Bereken het volume van een bol met straal r."""
+    volume = (4/3) * math.pi * (r ** 3)
+    return volume
 
 zijde = 5
 radius = 4
 
-print(kubus_vol(5))
-print(bol_vol(4))
+print(f"De inhoud van deze kubus is: {kubus_vol(zijde)}")
+print(f"De inhoud van deze bol is: {bol_vol(radius)}")
