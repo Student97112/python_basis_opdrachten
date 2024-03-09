6bf88c4b818852522ac4f056d274dab335a50523
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:14:49 +0100

    080 opdr 2

--- opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
+++ opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
@@ -8,3 +8,28 @@ geheim_getal = random.randint(1, 100)
 
 # De rest moet jij doen.....
 
+import random
+
+# Genereer een willekeurig getal tussen 1 en 100
+geheim_getal = random.randint(1, 100)
+
+# Initialisatie van het aantal pogingen
+aantal_pogingen = 0
+
+print("Raad mijn geheime getal")
+
+# Blijf vragen om een gok totdat de gebruiker het juiste getal heeft geraden
+while True:
+    gok = int(input())
+    aantal_pogingen += 1
+    
+    if gok < geheim_getal:
+        print("hoger")
+    elif gok > geheim_getal:
+        print("lager")
+    else:
+        print(f"Je hebt het getal geraden, het is {geheim_getal}!")
+        print(f"Je hebt het in {aantal_pogingen} keer gedaan")
+        break
+
+
