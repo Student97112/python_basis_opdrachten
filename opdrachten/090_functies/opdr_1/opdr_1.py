b496d586dbd6f73e01b3d48f4de3171d304c0543
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:18:01 +0100

    090 opdr1

--- opdrachten/090_functies/opdr_1/opdr_1.py
+++ opdrachten/090_functies/opdr_1/opdr_1.py
@@ -3,11 +3,12 @@
 # Groep:
 
 
-def write_to_file(afile, atext):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+def write_to_file(filename, text):
+    """Schrijf tekst naar een bestand."""
+    with open(filename, "a") as file:
+        file.write(text + "\n")
 
+# Voorbeeldgebruik van de functie
 my_tekst = "Schrijf dit maar even in een bestandje"
 my_file = "test.txt"
 write_to_file(my_file, my_tekst)
