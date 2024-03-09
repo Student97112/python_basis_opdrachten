4db42a542e8a68c368fd2a0e11490d33184cda75
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:07:37 +0100

    060 opdr4

--- opdrachten/060_loops/opdr_4/opdr_4.py
+++ opdrachten/060_loops/opdr_4/opdr_4.py
@@ -6,4 +6,8 @@
 
 # Hier start de for-loop
 
-my_list = []
+# Gebruik een lijstcomprehensie om de y-waarden te berekenen voor punten op een lijn
+y_waarden = [4 * x + 7 for x in range(1, 11)]
+
+# Print de lijst met y-waarden
+print(y_waarden)
