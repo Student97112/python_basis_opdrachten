061747bd342e86b76c3812bc072748b991b74a94
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:10:43 +0100

    070 opdr1

--- opdrachten/070_condities/opdr_1/opdr_1.py
+++ opdrachten/070_condities/opdr_1/opdr_1.py
@@ -6,7 +6,15 @@
 
 # Hier start de for-loop....
 
-my_list = []
+# Maak een lege lijst om getallen van 1 t/m 10 in op te slaan
+getallen = []
 
-#for loop
-#if statement
+# Vul de lijst met getallen van 1 t/m 10 met behulp van een loop
+for i in range(1, 11):
+    # Controleer of het getal groter is dan 4
+    if i > 4:
+        # Voeg het getal toe aan de lijst
+        getallen.append(i)
+
+# Print de getallen die groter zijn dan 4 op het scherm
+print(getallen)
