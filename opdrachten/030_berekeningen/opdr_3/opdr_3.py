a90707e2fb2ea79b3c65621fdbc445b1f9c0d42b
Student97112 <97112198@st.deltion.nl>
2024-03-09 14:58:31 +0100

    versie030 c

--- opdrachten/030_berekeningen/opdr_3/opdr_3.py
+++ opdrachten/030_berekeningen/opdr_3/opdr_3.py
@@ -3,14 +3,16 @@
 # Groep:
 
 # Hier komt je code...
-x = 1
-y = ...
-print( ... )
 
-x = 2
-y = ...
-print( ... )
+# Definieer de functie voor y in termen van x
+def bereken_y(x):
+    return 4 * x**3 - 2 * x**2 - 1
 
-x = 0
-y = ...
-print( ... )
+# Testgevallen
+x_waarden = [1, 2, 0]
+
+# Berekeningen en uitvoer voor elk testgeval
+for x in x_waarden:
+    y = bereken_y(x)
+    print(f"Bij x = {x}, is y = {y}")
+
