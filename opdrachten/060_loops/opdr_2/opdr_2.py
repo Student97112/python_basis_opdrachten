370306af8d7b87976428769e068aefd1c659c985
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:06:03 +0100

    060 opdr2

--- opdrachten/060_loops/opdr_2/opdr_2.py
+++ opdrachten/060_loops/opdr_2/opdr_2.py
@@ -6,5 +6,14 @@
 
 # Hier start de for-loop
 
-my_list = []
+
+# Maak een lege lijst om de resultaten op te slaan
+resultaten = []
+
+# Gebruik een for-loop en een range-functie om de gewenste lijst te genereren
+for i in range(1, 6):
+    resultaten.append(i * 10)
+
+# Print de gegenereerde lijst op het scherm
+print(resultaten)
 
