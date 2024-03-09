c040d35635559a2e40c214816421ffd0de771f0d
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:21:14 +0100

    090 opdr4

--- opdrachten/090_functies/opdr_4/opdr_4.py
+++ opdrachten/090_functies/opdr_4/opdr_4.py
@@ -4,11 +4,16 @@
 
 
 def volledige_naam(lijst_met_namen):
-    # hier komt jouw code
-    # Het woordje pass mag je weghalen
-    pass
+    """Print de volledige namen uit de lijst met namen."""
+    for naam in lijst_met_namen:
+        # Bouw de volledige naam op
+        volledige_naam = naam["voornaam"]
+        if naam["tussenvoegsel"]:
+            volledige_naam += " " + naam["tussenvoegsel"]
+        volledige_naam += " " + naam["achternaam"]
+        print(volledige_naam)
 
-
+# Lijst met namen
 namen = [
     {"voornaam": "Willem", "tussenvoegsel": "van", "achternaam": "Dijk"},
     {"voornaam": "Klaas", "tussenvoegsel": "", "achternaam": "Wopstra"},
@@ -16,4 +21,5 @@ namen = [
     {"voornaam": "Carla", "tussenvoegsel": "", "achternaam": "Hoogvliet"},
 ]
 
+# Roep de functie aan
 volledige_naam(namen)
