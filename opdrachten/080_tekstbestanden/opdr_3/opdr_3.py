d37a7970d81999636941ff6e5beb0a3874196580
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:15:46 +0100

    080 opdr3

--- opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
+++ opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
@@ -3,4 +3,25 @@
 # Groep:
 
 
+def encrypt(text):
+    encrypted_text = ''
+    for char in text:
+        if char.isalpha():  # Controleer of het een letter is
+            offset = 5
+            if char.islower():  # Controleer of het een kleine letter is
+                encrypted_char = chr(((ord(char) - 97 + offset) % 26) + 97)
+            else:  # Het is een hoofdletter
+                encrypted_char = chr(((ord(char) - 65 + offset) % 26) + 65)
+        else:  # Het is geen letter, behoud de originele karakter
+            encrypted_char = char
+        encrypted_text += encrypted_char
+    return encrypted_text
+
+# Invoer van de gebruiker
+text = input("Geef de tekst die je wilt encrypten: ")
+
+# Versleutel de tekst
+encrypted_text = encrypt(text)
+print("Versleutelde tekst:")
+print(encrypted_text)
 
