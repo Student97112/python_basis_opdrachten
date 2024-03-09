3ac5d320a004c646489d96c31b9d4e4d3b27b709
Student97112 <97112198@st.deltion.nl>
2024-03-09 14:59:51 +0100

    040 opdr1

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,18 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+# Maak een list aan om dictionaries op te slaan
+personen = []
+
+# Maak 4 dictionaries om gegevens van personen in op te slaan
+persoon1 = { "id": 1, "voornaam": "Jan", "achternaam": "Jansen" }
+persoon2 = { "id": 2, "voornaam": "Piet", "achternaam": "Pietersen" }
+persoon3 = { "id": 3, "voornaam": "Kees", "achternaam": "Klaassen" }
+persoon4 = { "id": 4, "voornaam": "Marie", "achternaam": "Janssen" }
+
+# Voeg de 4 dictionaries toe aan de list
+personen.extend([persoon1, persoon2, persoon3, persoon4])
+
+# Print de volledige naam van de 2e persoon op het scherm
+print(personen[1]["voornaam"], personen[1]["achternaam"])
+
