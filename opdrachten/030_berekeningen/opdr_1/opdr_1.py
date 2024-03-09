a6f11785ad55411469713f25beb0b57f3e5803e7
Student97112 <97112198@st.deltion.nl>
2024-03-09 14:55:35 +0100

    versie3c

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,18 @@
 # Groep:
 
 # Hier komt je code...
+# Maak variabelen voor de leeftijden van je beste vrienden
+joe = 23
+bob = 15
+fred = 22
+
+# Bereken de totale waarde van alle leeftijden bij elkaar opgeteld
+totaal_leeftijd = joe + bob + fred
+
+# Bereken de gemiddelde leeftijd van je beste vrienden
+gemiddelde_leeftijd = totaal_leeftijd / 3
+
+# Geef de gevraagde gegevens weer op het scherm
+print("Dit weten we over joe, bob en fred:")
+print("De totale waarde van alle leeftijden bij elkaar opgeteld =", totaal_leeftijd)
+print("De gemiddelde leeftijd van deze boys is:", gemiddelde_leeftijd)
