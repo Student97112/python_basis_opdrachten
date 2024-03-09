9b565c8132ef70b88987b711c6eaf6d7478e5da6
Student97112 <97112198@st.deltion.nl>
2024-03-09 15:19:21 +0100

    090 opdr2

--- opdrachten/090_functies/opdr_2/opdr_2.py
+++ opdrachten/090_functies/opdr_2/opdr_2.py
@@ -4,17 +4,17 @@
 
 
 def kilometers_naar_miles(km):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    """Converteer kilometers naar mijlen."""
+    miles = km / 1.609344
+    return miles
 
 def miles_naar_kilometers(miles):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    """Converteer mijlen naar kilometers."""
+    km = miles * 1.609344
+    return km
 
 kilometers = 1223
 miles = 867
 
-print(kilometers_naar_miles(kilometers))
-print(miles_naar_kilometers(miles))
+print(f"{kilometers} kilometers = {kilometers_naar_miles(kilometers)} miles")
+print(f"{miles} miles = {miles_naar_kilometers(miles)} kilometers")
